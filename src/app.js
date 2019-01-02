327b87321c92dd025975b1f6378c52b03cd6ed84
Ashwin Ramachandran <ashwinram1@gmail.com>
2019-01-02 14:28:35 -0800

    Add Expensify styles

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import {firebase} from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
 
@@ -27,15 +28,13 @@ const renderApp = () => {
     }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
-
-
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
     if(user) {
         //console.log('uid: ', user.uid);
         //redirect to dashboard page
-        store.dispatch(login(user.uid));
+        store.dispatch(login(user.uid));    
         store.dispatch(startSetExpenses()).then(() => {
             renderApp();
             if(history.location.pathname === '/') {
--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import expensesTotal from '../selectors/expenses-total';
 import getVisibleExpenses from '../selectors/expenses'; 
 import numeral from 'numeral';
 
 export const ExpensesSummary = (props) => {
     return (
-        <div>
-            <p>Viewing {props.expenseCount} {props.expenseCount === 1 ? 'expense':'expenses'} totalling {numeral(props.expensesTotal / 100).format('$0,0.00')}</p>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{props.expenseCount}</span> {props.expenseCount === 1 ? 'expense':'expenses'} totalling <span>{numeral(props.expensesTotal / 100).format('$0,0.00')}</span></h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
         </div>
     );
 };
